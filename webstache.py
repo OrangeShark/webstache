49a6a140c61b850606386179451d994e5261eb0d
Erik Edrosa <erik.edrosa@gmail.com>
2013-08-04 23:17:24 -0400

    Added main function and command line parsing

---
+++ webstache.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+
+import os, sys, getopt, argparse
+
+def main(argv):
+  parser = argparse.ArgumentParser(description='Generates static webpages based off of mustache templates')
+  parser.add_argument('directory', nargs='?', default=os.getcwd(), help='Directory of mustache files and data')
+  args = parser.parse_args()
+
+  print(args.directory)
+
+
+if __name__ == "__main__":
+  main(sys.argv[1:])

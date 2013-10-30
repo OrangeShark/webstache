1c2c2ed3b3b51f0399b137ad8dc248bdbde78827
Erik Edrosa <erik.edrosa@gmail.com>
2013-10-29 23:55:18 -0400

    Updated README and added glob

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 webstache
 =========
+webstache is a command line program which generates static webpages using mustache templates and json data files as input. Typing into the command line 
+'''bash
+webstache directory/to/input/files
+'''
+will create a directory called website with the generated HTML pages.
+
+This program is used as an example project for the FIU's Panther Linux User Group's Python workshop.
--- webstache.py
+++ webstache.py
@@ -1,13 +1,19 @@
 #!/usr/bin/python
 
 import os, sys, getopt, argparse
+import glob
 
 def main():
   parser = argparse.ArgumentParser(description='Generates static webpages based off of mustache templates')
   parser.add_argument('directory', nargs='?', default=os.getcwd(), help='Directory of mustache files and data')
   args = parser.parse_args()
-
+  
   print(args.directory)
+  templates = glob.glob(args.directory + "/*.mustache")
+  data = glob.glob(args.directory + "/*.json")
+
+  print(templates)
+  print(data)
 
 
 if __name__ == "__main__":

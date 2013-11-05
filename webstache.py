59d48a1c335fab25b94f26d063f2a0c8b9b8d249
Erik Edrosa <erik.edrosa@gmail.com>
2013-11-05 17:04:24 -0500

    Finished code from presentation

--- webstache.py
+++ webstache.py
@@ -2,6 +2,8 @@
 
 import os, sys, getopt, argparse
 import glob
+import pystache
+import json
 
 def main():
   parser = argparse.ArgumentParser(description='Generates static webpages based off of mustache templates')
@@ -9,11 +11,29 @@ def main():
   args = parser.parse_args()
   
   print(args.directory)
-  templates = glob.glob(args.directory + "/*.mustache")
+  template_file = open(args.directory + "/base.mustache", "r")
+  template = template_file.read()
   data = glob.glob(args.directory + "/*.json")
 
-  print(templates)
   print(data)
+
+  dataname = []
+  datafiles = []
+  for file in data:
+    dataname.append(os.path.split(file)[-1].split(".")[0])
+    currfile = open(file).read()
+    datafiles.append(json.loads(currfile))
+
+  print(dataname)
+
+  if not os.path.exists(args.directory + "/output"):
+    os.mkdir(args.directory + "/output")
+
+  i = 0
+  while i < len(datafiles):
+    htmlfile = open(args.directory + "/output/" + dataname[i] + ".html", "w")
+    htmlfile.write(pystache.render(template, datafiles[i]))
+    i += 1
 
 
 if __name__ == "__main__":

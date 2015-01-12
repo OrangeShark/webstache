26d42bd5ff3948d864ebd834463278013c669ddb
Erik Edrosa <erik.edrosa@gmail.com>
2015-01-12 14:40:51 -0500

    update copyright

--- webstache/page.py
+++ webstache/page.py
@@ -1,3 +1,21 @@
+# Page specific code
+# Copyright (C) 2013-2015 Erik Edrosa
+#
+# This file is part of webstache
+# 
+# webstache is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# webstache is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with webstache. If not, see <http://www.gnu.org/licenses/>.
+
 import os
 
 class Page:
